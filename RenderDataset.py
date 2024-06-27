2c32ab1a81ad5c54b40461ca4e235f5e719476ed
rainy_start@163.com <Rainy>
2024-06-27 23:56:51 +0800

    init

---
+++ RenderDataset.py
@@ -0,0 +1,27 @@
+import torch
+from torch.utils.data import Dataset
+import json
+class RenderDateset(Dataset):
+    def __init__(self,data_dir="a.json",transform=None) -> None:
+        super().__init__()
+        with open(data_dir) as f:
+            self.datas = json.load(f)
+        self.transform = transform
+    def __getitem__(self, index):
+        para = torch.Tensor(self.datas[index]["dir"] + self.datas[index]["point"])
+        label = torch.Tensor(self.datas[index]["rgb"])
+        if self.transform is not None:
+            para = self.transform(para)
+            label = self.transform(label)
+        return para,label
+    def __len__(self)->int:
+        return len(self.datas)
+
+## test
+
+if __name__ == "__main__":
+    from torch.utils.data import DataLoader
+    dataset = RenderDateset()
+    dataset_loader = DataLoader(dataset,batch_size=4,shuffle=True)
+    for batch in dataset_loader:
+        para,label = batch
